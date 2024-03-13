c6b32862bbfca338875fe0922cf34fb9f81e9d36
VRUJESH H M <60779508+VRU2306@users.noreply.github.com>
2024-03-13 20:28:56 +0530

    Inital second commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+require('dotenv').config();
+const express = require('express');
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Add middleware to parse json
+app.use(express.json());
+app.use(cors());
+
+app.all("/");
+const port = process.env.PORT || 5000;
+
+// Starting the Server 
+app.listen(port, () => {
+    console.log(`server connected to ${port}`)
+})
